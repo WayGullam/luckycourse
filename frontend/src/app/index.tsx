02e0da9f664efdad8d1b677a84726177b268697a
sovarim <abdul.taramov2001@gmail.com>
2024-08-03 16:10:25 +0300

    fix frontend run

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,6 +1,5 @@
 import { ToastContainer } from 'react-toastify';
 import { Providers } from './providers';
-import './styles/main.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
