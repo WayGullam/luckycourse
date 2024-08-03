022fbb0473393c0780bb4a7e72baebf9024ebd6a
sovarim <abdul.taramov2001@gmail.com>
2024-08-04 02:56:26 +0300

    course constructor

--- frontend-crm/src/main.tsx
+++ frontend-crm/src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { App } from '@/app';
+import 'react-image-picker-editor/dist/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
