d2c04190ba73022d7321825cd42c031564f8b4f8
Zhebarov <xarcho.ev@gmail.com>
2024-08-04 02:08:32 +0300

    начал вывод модулей и уроков внутри

--- frontend/src/pages/CoursePage/ui/CoursePage.tsx
+++ frontend/src/pages/CoursePage/ui/CoursePage.tsx
@@ -1,11 +1,18 @@
 import { useParams } from 'react-router-dom';
 import { useGetCourseByIdQuery } from '@/api/courseApi.ts';
 
+import styles from './CoursePage.module.scss';
+import { useState } from 'react';
 
 const CoursePage = () => {
   const { courseId } = useParams();
-    console.log(courseId)
+  console.log(courseId);
   const { data, error, isLoading } = useGetCourseByIdQuery(courseId);
+  const [openModuleId, setOpenModuleId] = useState(null);
+
+  const handleOpenModule = (id) => {
+    setOpenModuleId((prevId) => (prevId === id ? null : id)); // Открыть модуль или закрыть, если тот же модуль был открыт
+  };
 
   if (isLoading) return <div>{'Загрузка...'}</div>;
 
@@ -13,6 +20,26 @@ const CoursePage = () => {
     <div>
       <h1>{data?.title}</h1>
       <p>{data?.description}</p>
+      <ul className={styles.modulesList}>
+        {data?.modules.map((module) => (
+          <li
+            key={module.id}
+            className={styles.moduleItem}
+            onClick={() => handleOpenModule(module.id)}
+          >
+            {module.title}
+            {openModuleId === module.id && (
+              <ul className={styles.moduleLessons}>
+                {module.lessons?.map((lesson) => (
+                  <li key={lesson.id} className={styles.lessonItem}>
+                    {lesson.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
