c658ffa41bf25dee7ef875e074431507b6ac2bd8
wulfram <wulfram4@gmail.com>
2024-08-04 14:00:59 +0300

    add backend and remove not used code

--- backend/core/filters.py
+++ backend/core/filters.py
@@ -1,30 +1,31 @@
 import django_filters
-from core.models import Category, Ingredient, Client
+
+# from core.models import Category, Ingredient, Client
 
 
-class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
-    pass
+# class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
+#     pass
 
 
-class CategoriesFilter(django_filters.FilterSet):
-    categories = NumberInFilter(field_name='id', lookup_expr='in')
+# class CategoriesFilter(django_filters.FilterSet):
+#     categories = NumberInFilter(field_name='id', lookup_expr='in')
 
-    class Meta:
-        model = Category
-        fields = ['id']
-
-
-class IngredientsFilter(django_filters.FilterSet):
-    ingredients = NumberInFilter(field_name='id', lookup_expr='in')
-
-    class Meta:
-        model = Ingredient
-        fields = ['id']
+#     class Meta:
+#         model = Category
+#         fields = ['id']
 
 
-class ClientFilter(django_filters.FilterSet):
-    client_id = NumberInFilter(field_name='id', lookup_expr='in')
+# class IngredientsFilter(django_filters.FilterSet):
+#     ingredients = NumberInFilter(field_name='id', lookup_expr='in')
 
-    class Meta:
-        model = Client
-        fields = ['id']
+#     class Meta:
+#         model = Ingredient
+#         fields = ['id']
+
+
+# class ClientFilter(django_filters.FilterSet):
+#     client_id = NumberInFilter(field_name='id', lookup_expr='in')
+
+#     class Meta:
+#         model = Client
+#         fields = ['id']
