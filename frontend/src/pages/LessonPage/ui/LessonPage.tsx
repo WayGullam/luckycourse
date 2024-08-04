6211f29e8869f3c7c069b62974f08f220a48ed6f
SaydAliUsmanov <usmanov.guno@mail.ru>
2024-08-04 08:26:17 +0300

    added innerHtml

--- frontend/src/pages/LessonPage/ui/LessonPage.tsx
+++ frontend/src/pages/LessonPage/ui/LessonPage.tsx
@@ -4,25 +4,32 @@ import styles from './LessonPage.module.scss';
 
 const LessonPage = () => {
   const location = useLocation();
-  const lesson = location.state as { id: number; title: string; description: string; content: string };
+  const lesson = location.state as {
+    id: number;
+    title: string;
+    description: string;
+    content: string;
+  };
 
   if (!lesson) return <div>{'Урок не найден'}</div>;
 
   return (
-    <Container maxWidth="md" className={styles.lessonContainer}>
+    <Container maxWidth='md' className={styles.lessonContainer}>
       <Paper elevation={3} className={styles.lessonPaper}>
         <Box className={styles.lessonHeader}>
-          <Typography variant="h2" className={styles.lessonTitle}>
+          <Typography variant='h2' className={styles.lessonTitle}>
             {lesson.title}
           </Typography>
-          <img src="/path/to/illustration.png" alt="Illustration" className={styles.lessonIllustration} />
+          <img
+            src='/path/to/illustration.png'
+            alt='Illustration'
+            className={styles.lessonIllustration}
+          />
         </Box>
-        <Typography variant="body1" className={styles.lessonDescription}>
+        <Typography variant='body1' className={styles.lessonDescription}>
           {lesson.description}
         </Typography>
-        <Typography variant="body2" className={styles.lessonContent}>
-          {lesson.content}
-        </Typography>
+        <div dangerouslySetInnerHTML={{ __html: lesson.content }} />
       </Paper>
     </Container>
   );
